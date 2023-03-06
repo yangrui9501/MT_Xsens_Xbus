90d645c8d64165dcaa16cd4b2a13e4e008400de4
yangrui9501 <yang77567789@gmail.com>
2023-03-06 15:46:43 +0800

    1

--- example_utility/mti600_get_data.hpp
+++ example_utility/mti600_get_data.hpp
@@ -14,14 +14,20 @@
 #include <Arduino.h>
 #include <xbus.h>
 
-#define XBUS_DATA_CONFIG_GNSS_PVT 0
-#define XBUS_DATA_CONFIG_INS 0
-#define XBUS_DATA_CONFIG_ORIENTATION 1
-#define XBUS_DATA_CONFIG_IMU 1
-#define XBUS_DATA_CONFIG_BARO 1
+
+
+#define XBUS_DATA_CONFIG_QUATERNION 1
+#define XBUS_DATA_CONFIG_RATE_OF_TURN 0
+#define XBUS_DATA_CONFIG_ACCEL 0
 #define XBUS_DATA_CONFIG_FREE_ACCEL 1
-#define XBUS_DATA_CONFIG_DELTA_V 0
+#define XBUS_DATA_CONFIG_MAGNETIC_FIELD 1
 #define XBUS_DATA_CONFIG_TEMPERATURE 1
+#define XBUS_DATA_CONFIG_BARO 1
+#define XBUS_DATA_CONFIG_ACCEL_HR 1
+#define XBUS_DATA_CONFIG_RATE_OF_TURN_HR 1
+#define XBUS_DATA_CONFIG_POSITION_AND_VELOCITY 1
+#define XBUS_DATA_CONFIG_GNSS_DATA 1
+
 
 namespace xsens
 {
@@ -29,7 +35,50 @@ template <class T>
 struct xbus_motion_data
 {
     xbus_motion_data() { memset(this, 0, sizeof(xbus_motion_data)); }
-#if XBUS_DATA_CONFIG_GNSS_PVT
+
+#if XBUS_DATA_CONFIG_QUATERNION
+    T quat[4];
+    T euler[3];
+#endif
+
+#if XBUS_DATA_CONFIG_RATE_OF_TURN
+    T gyro[3];
+#endif
+#if XBUS_DATA_CONFIG_ACCEL
+    T accel[3];
+#endif
+
+#if XBUS_DATA_CONFIG_MAGNETIC_FIELD
+    T mag[3];
+#endif
+
+#if XBUS_DATA_CONFIG_TEMPERATURE
+    T temperature;
+#endif
+
+#if XBUS_DATA_CONFIG_BARO
+    uint32_t baro;
+#endif
+
+#if XBUS_DATA_CONFIG_ACCEL_HR
+    float accel_hr[3];
+#endif
+
+#if XBUS_DATA_CONFIG_RATE_OF_TURN_HR
+    float gyro_hr[3];
+#endif
+
+#if XBUS_DATA_CONFIG_FREE_ACCEL
+    T free_accel[3];
+#endif
+
+#if XBUS_DATA_CONFIG_POSITION_AND_VELOCITY
+    T latlon[2];
+    T altitude;
+    T velocity[3];
+#endif
+
+#if XBUS_DATA_CONFIG_GNSS_DATA
     uint16_t year;
     uint8_t month;
     uint8_t day;
@@ -39,39 +88,6 @@ struct xbus_motion_data
     int32_t lon;
     int32_t lat;
     int32_t height;
-#endif
-
-#if XBUS_DATA_CONFIG_INS
-    T latlon[2];
-    T altitude;
-    T velocity[3];
-#endif
-
-#if XBUS_DATA_CONFIG_ORIENTATION
-    T quat[4];
-    T euler[3];
-#endif
-
-#if XBUS_DATA_CONFIG_IMU
-    T gyro[3];
-    T accel[3];
-    T mag[3];
-#endif
-
-#if XBUS_DATA_CONFIG_BARO
-    uint32_t baro;
-#endif
-
-#if XBUS_DATA_CONFIG_FREE_ACCEL
-    T free_accel[3];
-#endif
-
-#if XBUS_DATA_CONFIG_DELTA_V
-    T delta_v[3];
-#endif
-
-#if XBUS_DATA_CONFIG_TEMPERATURE
-    T temperature;
 #endif
 };
 typedef struct xbus_motion_data<float> xbus_motion_data_float;
@@ -120,7 +136,48 @@ void euler_to_quat(double(&euler321)[3], double(&quat)[4])
 
 void xbus_get_all_data(xsens::Xbus& xbus, xsens::xbus_motion_data_double& data)
 {
-#if XBUS_DATA_CONFIG_GNSS_PVT
+
+#if XBUS_DATA_CONFIG_QUATERNION
+    memcpy(data.quat, xbus.quat.f64, sizeof(data.quat));
+    quat_to_euler(data.quat, data.euler);
+#endif
+
+#if XBUS_DATA_CONFIG_RATE_OF_TURN
+    memcpy(data.gyro, xbus.gyro.f64, sizeof(data.gyro));
+#endif
+#if XBUS_DATA_CONFIG_ACCEL
+    memcpy(data.accel, xbus.accel.f64, sizeof(data.accel));
+#endif
+#if XBUS_DATA_CONFIG_FREE_ACCEL
+    memcpy(data.free_accel, xbus.free_accel.f64, sizeof(data.free_accel));
+#endif
+#if XBUS_DATA_CONFIG_MAGNETIC_FIELD
+    memcpy(data.mag, xbus.mag.f64, sizeof(data.mag));
+#endif
+
+#if XBUS_DATA_CONFIG_TEMPERATURE
+    data.temperature = xbus.temperature.f64;
+#endif
+
+#if XBUS_DATA_CONFIG_BARO
+    data.baro = xbus.baro;
+#endif
+
+#if XBUS_DATA_CONFIG_ACCEL_HR
+    memcpy(data.accel_hr, xbus.accel_hr, sizeof(data.accel_hr));
+#endif
+
+#if XBUS_DATA_CONFIG_RATE_OF_TURN_HR
+    memcpy(data.gyro_hr, xbus.gyro_hr, sizeof(data.gyro_hr));
+#endif
+
+#if XBUS_DATA_CONFIG_POSITION_AND_VELOCITY
+    memcpy(data.latlon, xbus.latlon.f64, sizeof(data.latlon));
+    memcpy(&data.altitude, &xbus.altitude.f64, sizeof(data.altitude));
+    memcpy(data.velocity, &xbus.velocity.f64, sizeof(data.velocity));
+#endif
+
+#if XBUS_DATA_CONFIG_GNSS_DATA
     data.year = xbus.get_gnss().get_pvt().year;
     data.month = xbus.get_gnss().get_pvt().month;
     data.day = xbus.get_gnss().get_pvt().day;
@@ -130,39 +187,6 @@ void xbus_get_all_data(xsens::Xbus& xbus, xsens::xbus_motion_data_double& data)
     data.lat = xbus.get_gnss().get_pvt().lat;
     data.lon = xbus.get_gnss().get_pvt().lon;
     data.height = xbus.get_gnss().get_pvt().height;
-#endif
-
-#if XBUS_DATA_CONFIG_INS
-    memcpy(data.latlon, xbus.latlon.f64, sizeof(data.latlon));
-    memcpy(&data.altitude, &xbus.altitude.f64, sizeof(data.altitude));
-    memcpy(data.velocity, &xbus.velocity.f64, sizeof(data.velocity));
-#endif
-
-#if XBUS_DATA_CONFIG_ORIENTATION
-    memcpy(data.quat, xbus.quat.f64, sizeof(data.quat));
-    quat_to_euler(data.quat, data.euler);
-#endif
-
-#if XBUS_DATA_CONFIG_IMU
-    memcpy(data.gyro, xbus.gyro.f64, sizeof(data.gyro));
-    memcpy(data.accel, xbus.accel.f64, sizeof(data.accel));
-    memcpy(data.mag, xbus.mag.f64, sizeof(data.mag));
-#endif
-
-#if XBUS_DATA_CONFIG_BARO
-    data.baro = xbus.baro;
-#endif
-
-#if XBUS_DATA_CONFIG_FREE_ACCEL
-    memcpy(data.free_accel, xbus.free_accel.f64, sizeof(data.free_accel));
-#endif
-
-#if XBUS_DATA_CONFIG_DELTA_V
-    memcpy(data.delta_v, xbus.delta_v.f64, sizeof(data.delta_v));
-#endif
-
-#if XBUS_DATA_CONFIG_DELTA_V
-    data.temperature = xbus.temperature.f64;
 #endif
 }
 
@@ -179,21 +203,49 @@ void print_quaternion(xsens::xbus_motion_data_double& data)
     Serial.println(")");
 }
 
-void print_imu(xsens::xbus_motion_data_double& data)
+#if BUS_DATA_CONFIG_RATE_OF_TURN
+void print_rate_of_turn(xsens::xbus_motion_data_double& data)
 {
-    Serial.print("Gyro: (");
+    Serial.print("Rate of Turn (rad/s): (");
     Serial.print(data.gyro[0]);
     Serial.print(", ");
     Serial.print(data.gyro[1]);
     Serial.print(", ");
     Serial.print(data.gyro[2]);
-    Serial.print(") | Accel: (");
+    Serial.print(")");
+}
+#endif 
+
+#if XBUS_DATA_CONFIG_ACCEL
+void print_accel(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Accel (m/ss): (");
     Serial.print(data.accel[0]);
-    Serial.print(",");
+    Serial.print(", ");
     Serial.print(data.accel[1]);
     Serial.print(", ");
     Serial.print(data.accel[2]);
-    Serial.print(") | Mag: (");
+    Serial.println(")");
+}
+#endif
+
+#if XBUS_DATA_CONFIG_FREE_ACCEL
+void print_free_accel(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Free Accel (m/ss): (");
+    Serial.print(data.free_accel[0]);
+    Serial.print(", ");
+    Serial.print(data.free_accel[1]);
+    Serial.print(", ");
+    Serial.print(data.free_accel[2]);
+    Serial.println(")");
+}
+#endif
+
+#if XBUS_DATA_CONFIG_MAGNETIC_FIELD
+void print_magnetic_field(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Mag: (");
     Serial.print(data.mag[0]);
     Serial.print(", ");
     Serial.print(data.mag[1]);
@@ -201,11 +253,56 @@ void print_imu(xsens::xbus_motion_data_double& data)
     Serial.print(data.mag[2]);
     Serial.println(")");
 }
+#endif
 
-#if XBUS_DATA_CONFIG_INS
+#if XBUS_DATA_CONFIG_TEMPERATURE 
+void print_temperature(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Temperature (C): ");
+    Serial.print(data.temperature);
+    Serial.println();
+}
+#endif 
+
+#if XBUS_DATA_CONFIG_BARO
+void print_baro(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Barometric Pressure (Pa): ");
+    Serial.print(data.baro);
+    Serial.println();
+}
+#endif
+
+#if XBUS_DATA_CONFIG_ACCEL_HR 
+void print_accel_hr(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Accel (HR)(m/ss): (");
+    Serial.print(data.accel_hr[0]);
+    Serial.print(", ");
+    Serial.print(data.accel_hr[1]);
+    Serial.print(", ");
+    Serial.print(data.accel_hr[2]);
+    Serial.println(")");
+}
+#endif 
+
+#if XBUS_DATA_CONFIG_RATE_OF_TURN_HR 
+void print_rate_of_turn_hr(xsens::xbus_motion_data_double& data)
+{
+    Serial.print("Rate of Turn (HR)(m/ss): (");
+    Serial.print(data.gyro_hr[0]);
+    Serial.print(", ");
+    Serial.print(data.gyro_hr[1]);
+    Serial.print(", ");
+    Serial.print(data.gyro_hr[2]);
+    Serial.println(")");
+}
+#endif 
+
+#if XBUS_DATA_CONFIG_POSITION_AND_VELOCITY
 void print_ins(xsens::xbus_motion_data_double& data)
 {
-    Serial.print("LLH: (");
+    Serial.print("INS Data | LLH: (");
     Serial.print(data.latlon[0]);
     Serial.print(", ");
     Serial.print(data.latlon[1]);
@@ -217,14 +314,14 @@ void print_ins(xsens::xbus_motion_data_double& data)
     Serial.print(data.velocity[1]);
     Serial.print(", ");
     Serial.print(data.velocity[2]);
-    Serial.print(")");
+    Serial.println(")");
 }
 #endif
 
-#if XBUS_DATA_CONFIG_GNSS_PVT
+#if XBUS_DATA_CONFIG_GNSS_DATA
 void print_gnss(xsens::xbus_motion_data_double& data)
 {
-    Serial.print("MTi GPS Data | ");
+    Serial.print("Pvt Data | ");
     Serial.print(data.year);
     Serial.print("-");
     Serial.print(data.month);
